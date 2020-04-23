6489cd5bcd5cf59bf88865415c1bc3ca3842ece2
Haofan Fang <haofanfang@gmail.com>
2020-04-23 21:44:35 +0800

    update GalleryViewModel

--- app/src/main/java/com/supremesir/gallerydemokt/GalleryViewModel.kt
+++ app/src/main/java/com/supremesir/gallerydemokt/GalleryViewModel.kt
@@ -3,11 +3,14 @@ package com.supremesir.gallerydemokt
 import android.app.Application
 import android.app.DownloadManager
 import android.content.ClipData
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.android.volley.Request
+import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
+import com.google.gson.Gson
 
 /**
  * @author HaoFan Fang
@@ -19,7 +22,27 @@ class GalleryViewModel(application: Application) : AndroidViewModel(application)
     // 通过不可改变的 LiveData 获取 MutableLiveData，实现数据封装
     private val _photoListLive = MutableLiveData<List<PhotoItem>>()
     val photoListLive: LiveData<List<PhotoItem>>
-    get() = _photoListLive
+        get() = _photoListLive
 
+    fun fetchData() {
+        val stringRequest = StringRequest(
+            Request.Method.GET,
+            getUrl(),
+            Response.Listener {
+                _photoListLive.value = Gson().fromJson(it, Pixabay::class.java).hits.toList()
+            },
+            Response.ErrorListener {
+                Log.d("error", it.toString())
+            }
+        ).also {
+            VolleySingleton.getInstance(getApplication()).requestQueue.add(it)
+        }
+    }
+
+    private fun getUrl(): String {
+        return "https://pixabay.com/api/?key=16144591-adae3cf7f07751722a20825cf&q=${keyWords.random()}"
+    }
+
+    private val keyWords = arrayOf("cat", "dog", "car", "bee", "phone", "flower", "animal")
 
 }
