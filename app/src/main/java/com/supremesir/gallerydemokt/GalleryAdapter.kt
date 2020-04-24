f4f1cb848961502f0022da84c8f250c32eafa2af
Haofan Fang <haofanfang@gmail.com>
2020-04-24 16:55:11 +0800

    update GalleryAdapter

---
+++ app/src/main/java/com/supremesir/gallerydemokt/GalleryAdapter.kt
@@ -0,0 +1,87 @@
+package com.supremesir.gallerydemokt
+
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import kotlinx.android.synthetic.main.gallery_cell.view.*
+
+
+/**
+ * @author HaoFan Fang
+ * @date 2020/4/23 21:54
+ */
+
+class GalleryAdapter : ListAdapter<PhotoItem, MyViewHolder>(DIFFCALLBACK) {
+
+    object DIFFCALLBACK : DiffUtil.ItemCallback<PhotoItem>() {
+        override fun areItemsTheSame(oldItem: PhotoItem, newItem: PhotoItem): Boolean {
+            // === 表示判断是否是同一个对象
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: PhotoItem, newItem: PhotoItem): Boolean {
+            return oldItem.photoId == newItem.photoId
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val holder = MyViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.gallery_cell, parent, false)
+        )
+        holder.itemView.setOnClickListener {  }
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.itemView.shimmerLayoutCell.apply {
+            setShimmerColor(0x55FFFFFF)
+            setShimmerAngle(0)
+            startShimmerAnimation()
+        }
+        Glide.with(holder.itemView)
+            .load(getItem(position).previewUrl)
+            .placeholder(R.drawable.ic_photo_black_24dp)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    // 必须返回false，否则无法显示图片
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+
+                    return false.also {
+                        // 加入 ?. 判空，
+                        holder.itemView.shimmerLayoutCell?.stopShimmerAnimation()
+                    }
+                }
+
+            })
+            .into(holder.itemView.imageView)
+
+    }
+
+}
+
+class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
