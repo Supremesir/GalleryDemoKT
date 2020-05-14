435671a81bc02f2bf3615542d9dde1b1e2b6eccb
Haofan Fang <haofanfang@gmail.com>
2020-05-14 11:40:07 +0800

    update request write_external_storage permission

--- app/src/main/java/com/supremesir/gallerydemokt/PagerPhotoFragment.kt
+++ app/src/main/java/com/supremesir/gallerydemokt/PagerPhotoFragment.kt
@@ -1,16 +1,24 @@
 package com.supremesir.gallerydemokt
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.fragment_pager_photo.*
 
 /**
  * A simple [Fragment] subclass.
  */
+
+const val REQUEST_WRITE_EXTERNAL_STORAGE = 1
+
 class PagerPhotoFragment : Fragment() {
 
     override fun onCreateView(
@@ -32,16 +40,49 @@ class PagerPhotoFragment : Fragment() {
         viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-                photoTag.text = "${position + 1}/${photoList?.size}"
+                photoTag.text = getString(R.string.photo_tag, position + 1, photoList?.size)
             }
         })
 
         // 保证 大图加载的是点击的小图，而不是从第一个显示
-        arguments?.getInt("PHOTO_POSITION")?.let {
-            // false 关闭平滑滚动的效果
-            viewPager2.setCurrentItem(it, false)
-        }
+        viewPager2.setCurrentItem(arguments?.getInt("PHOTO_POSITION") ?: 0, false)
+//        arguments?.getInt("PHOTO_POSITION")?.let {
+//            // false 关闭平滑滚动的效果
+//            viewPager2.setCurrentItem(it, false)
+//        }
 
+        saveButton.setOnClickListener {
+            if (Build.VERSION.SDK_INT < 29 && ContextCompat.checkSelfPermission(
+                    requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissions(
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    REQUEST_WRITE_EXTERNAL_STORAGE
+                )
+            } else {
+                savePhoto()
+            }
+        }
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_WRITE_EXTERNAL_STORAGE ->
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    savePhoto()
+                } else {
+                    Toast.makeText(requireContext(),"请授权存储权限以保存图片", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+    private fun savePhoto() {
+        TODO("Not yet implemented")
+    }
 }
